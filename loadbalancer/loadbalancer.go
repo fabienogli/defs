1ed23a65bab3c9207a1f1a0936ebe14fe437a8a2
fabienogli <fabien.ogli@etu.univ-lyon1.fr>
2019-05-20 15:27:11 -0400

    udp listening

--- loadbalancer/loadbalancer.go
+++ loadbalancer/loadbalancer.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"net"
 	"log"
 	"loadbalancer/database"
 	"errors"
+	"os"
 )
 
 func main() {
-	log.Println("load balancer running")
+	port := os.Getenv("port")
+	if port == "" {
+		panic("Port wasn't found\n")
+	}
+	port = ":" + port
+	log.Printf("Listening on %s\n", port)
+	serverConn, _ := net.ListenUDP(
+		"udp", 
+		&net.UDPAddr{
+			IP:[]byte{0,0,0,0},
+			Port:10001,
+			Zone:"",
+			},
+	)
+	defer serverConn.Close()
+	buf := make([]byte, 1024)
+	for {
+		n, addr, _ := serverConn.ReadFromUDP(buf)
+		log.Println("Received ", string(buf[0:n]), " from ", addr)
+	}
 }
 
 //get the location of the file
@@ -19,7 +40,6 @@ func whereIs(salt string) (database.Storage, error) {
 func whereTo(salt string, size int) (database.Storage, error) {
 	return database.Storage{}, errors.New("Not implemented")
 }
-
 
 func delete(salt string) error {
 	return errors.New("Not implemented")
