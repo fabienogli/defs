8ccd7aa46fc58195276ce93490e8c65b60f58f0d
Mathieu Regnard <mathieu.regnard2@gmail.com>
2019-06-10 14:43:23 -0400

    Refactored sanitarized string test

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -13,43 +13,18 @@ import (
 
 func TestSanitarizeString(t *testing.T) {
 	healthyString := "ThisIsAHealthyLittleString"
-	sanitarized := sanitarizeString(healthyString)
+	testSubSanitarize(t, healthyString, healthyString)
+	testSubSanitarize(t, "This Is A String With Space", "ThisIsAStringWithSpace")
+	testSubSanitarize(t, "This.Is.A.String.With.Dots", "ThisIsAStringWithDots")
+	testSubSanitarize(t, "This/Is/A/String/With/Slash", "ThisIsAStringWithSlash")
+	testSubSanitarize(t, "This Is A ..String/With./Lots Of Bad ...././../Stuff", "ThisIsAStringWithLotsOfBadStuff")
+}
 
-	if healthyString != sanitarized {
-		t.Errorf("Nothing should have been removed, " +
-			"healty is \"%s\", sanitarized was \"%s\"\n", healthyString, sanitarized)
-	}
+func testSubSanitarize(t *testing.T, input string, expected string) {
+	sanitarized := sanitarizeString(input)
 
-	stringWithSpace := "This Is A String With Space"
-	expected := "ThisIsAStringWithSpace"
-	sanitarized = sanitarizeString(stringWithSpace)
-
-	if sanitarized != expected {
-		t.Errorf("didn't remove spaces, expected \"%s\", got \"%s\"", expected, sanitarized)
-	}
-
-	stringWithDots := "This.Is.A.String.With.Dots"
-	expected = "ThisIsAStringWithDots"
-	sanitarized = sanitarizeString(stringWithDots)
-
-	if sanitarized != expected {
-		t.Errorf("didn't remove dots, expected \"%s\", got \"%s\"", expected, sanitarized)
-	}
-
-	stringWithSlashes := "This/Is/A/String/With/Slash"
-	expected  = "ThisIsAStringWithSlash"
-	sanitarized = sanitarizeString(stringWithSlashes)
-
-	if sanitarized != expected {
-		t.Errorf("didn't remove slashes, expected \"%s\", got \"%s\"", expected, sanitarized)
-	}
-
-	stringWithAll := "This Is A ..String/With./Lots Of Bad ...././../Stuff"
-	expected = "ThisIsAStringWithLotsOfBadStuff"
-	sanitarized = sanitarizeString(stringWithAll)
-
-	if sanitarized != expected {
-		t.Errorf("didnt sanitarize at all, expected \"%s\", got \"%s\"", expected, sanitarized)
+	if expected != sanitarized {
+		t.Errorf("sanitarization failed : expected %s, got %s", expected, sanitarized)
 	}
 }
 
