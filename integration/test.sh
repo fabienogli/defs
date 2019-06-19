8b70be268ca73f94eb117faf9ad0f854b2b8004b
fabienogli <fabien.ogli@etu.univ-lyon1.fr>
2019-06-19 18:45:02 -0400

    integration test + change cli to split building and unit testing

--- integration/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-FILENAME='test'
-CONTENT='This is a test'
-echo  $CONTENT > $FILENAME
-sha256sum $FILENAME > "$FILENAME.sha256sum"
-RESULT=$(curl -X POST -F filename=hello.txt -F file=@$FILENAME localhost:8080/file)
-echo $RESULT
---
+++ integration/upload.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+## UPLOADING FILE
+FILENAME='test'
+CONTENT='This is a test'
+echo  $CONTENT > $FILENAME
+OG_HASH=$(sha256sum $FILENAME)
+RESULT=$(curl -X POST -F filename=$FILENAME -F file=@$FILENAME localhost:8080/file)
+KEY=$(echo $RESULT | cut -d ":" -f 1 | tr -d \" | tr -d \{)
+RESPONSE=$(echo $RESULT | cut -d ":" -f 2 | tr -d \" | tr -d \})
+
+echo "key is $KEY"
+echo "response is $RESPONSE"
+if [[ $KEY != "hash" ]];then
+    echo "Not the good key, here is the full response"
+    echo $RESULT
+    exit 1
+fi
+
+## DOWNLOADING FILE
+OUTPUT_FILE="output"
+curl -X POST -F filename=$FILENAME -F file=@$FILENAME localhost:8080/file/$RESPONSE > $OUTPUT_FILE
+NEW_HASH=$(sha256sum $OUTPUT_FILE)
+if [[ NEW_HASH != OG_HASH ]]; then
+    echo "Different hash for same file, file is \n"
+    echo $OUTPUT_FILE
+    exit 1
+fi
+exit 0
+
