eae82da4ab806e7032e823574378e9e7a41119a4
fabienogli <fabien.ogli@etu.univ-lyon1.fr>
2019-06-20 16:22:08 -0400

    script should be good

--- integration/upload_tmp.sh
+++ integration/upload_tmp.sh
@@ -10,12 +10,12 @@ echo  $CONTENT > $FILENAME
 OG_HASH=$(sha256sum $FILENAME | cut -d " " -f 1)
 
 echo "Uploading the file"
-RESULT=$(curl -X POST -F filename=$FILENAME -F file=@$FILENAME --silent $ADDRESS)
+RESULT=$(curl -X POST -F ttl="1 minute" -F filename=$FILENAME -F file=@$FILENAME --silent $ADDRESS)
 
 rm $FILENAME
 
 KEY=$(echo $RESULT | cut -d ":" -f 1 | tr -d \" | tr -d \{)
-RESPONSE=$(echo $RESULT | cut -d ":" -f 2 | tr -d \" | tr -d \})
+RETRIEVED_HASH=$(echo $RESULT | cut -d ":" -f 2 | tr -d \" | tr -d \})
 
 if [[ $KEY != "hash" ]];then
     echo "Not the good key, here is the full response"
@@ -25,7 +25,7 @@ fi
 
 ## DOWNLOADING FILE
 OUTPUT_FILE="output"
-curl --silent $ADDRESS/$RESPONSE > $OUTPUT_FILE
+curl --silent $ADDRESS/$RETRIEVED_HASH > $OUTPUT_FILE
 
 NEW_HASH=$(sha256sum $OUTPUT_FILE | cut -d " " -f 1)
 
@@ -37,5 +37,18 @@ if [[ $NEW_HASH != $OG_HASH ]]; then
     echo "New hash: $NEW_HASH"
     exit 1
 fi
-exit 0
 
+# Waiting for the ttl to end
+echo "Waiting for the ttl to end"
+sleep 61
+RESULT=$(curl --silent $ADDRESS/$RETRIEVED_HASH)
+KEY=$(echo $RESULT | cut -d ":" -f 1 | tr -d \" | tr -d \{)
+MSG=$(echo $RESULT | cut -d ":" -f 2 | tr -d \" | tr -d \})
+
+if [[ $KEY != "error" ]]; then
+    echo "there should be an error here instead have"
+    echo $MSG
+    exit 1
+fi
+
+exit 0
